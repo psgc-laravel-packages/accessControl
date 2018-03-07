05bf928536461e5d7c241887e6d5acbd833d1b7f
Peter Gorgone <peter@peltronic.com>
2018-03-07 11:42:07 -0800

    fixes

--- src/AccessManager.php
+++ src/AccessManager.php
@@ -3,26 +3,18 @@ namespace PsgcLaravelPackages\AccessControl;
 
 use Closure;
 
-// %TODO: scrap this and try to go with only Entrust (!)
-// See: 
-// ~ => https://laravel.com/docs/5.6/authentication#protecting-routes
-// ~ https://laravel.com/docs/5.6/authorization
-// ~ https://github.com/Zizaco/entrust
-// ~ https://stackoverflow.com/questions/33439193/in-laravel-should-i-check-for-permission-in-controller-if-already-checking-on-m?rq=1
-// ~ https://github.com/spatie/laravel-authorize
 abstract class AccessManager
 {
-
     // %REQUIRES: User model implement ->roles attribute (eg, relations)
     // %REQUIRES: User model implement ->username // %TODO: make a function so it's generic returns some kind of GUID, or a GUID
-    //
+
     protected $_sessionUser = null;
     protected $_superadminRoles = []; // roles that override access matrix checking
 
     abstract protected function accessMatrix(); 
 
     // Optional: override in child class to add superadmin roles
-    protected function setSuperadminRoles();
+    protected function setSuperadminRoles()
     {
         $this->_superadminRoles = []; // default is none
     }
@@ -39,12 +31,11 @@ abstract class AccessManager
         $this->setSuperadminRoles();
 
         $this->_sessionUser = $sessionUser = \Auth::user();
-        $roles = $sessionUser->roles;
 
         $isSuperadmin = false; // default
-        foreach ($roles as $userRole) {
+        foreach ($sessionUser->roles as $r) {
             // Check if session user has a superadmin role...
-            if ( in_array($userRole,$this->_superadminRoles) ) {
+            if ( in_array($r->name,$this->_superadminRoles) ) {
                 $isSuperadmin = true;
                 break;
             }
@@ -54,33 +45,24 @@ abstract class AccessManager
             // Not a super admin, so use access matrix
             $accessMatrix = $this->accessMatrix();
     
-            //$tmp = $request->route()->parameters();
-            //dd($tmp);
-            //dd($tmp->parameters());
-            //dd($request->route('contractors'));
-            //$slug = $request->slug; //  get parameter from request (eg slug)
-            //$route = $request->route;
             $route = $request->route()->getName();
             $routeParams = $request->route()->parameters(); // for HTTP only not for AJAX
-            $queryParams = \Input::all();
+            $queryParams = $request->all();
     
-            //dd($route);
             //dd('This is the route ' . $route, $accessMatrix);
-            $accessLevel = null; // Default is to deny access
-            foreach ($roles as $role) {
-            if ( array_key_exists($route,$accessMatrix) ) {
-                    $accessLevel = !empty($accessMatrix[$route][$role->name]) ? $accessMatrix[$route][$role->name] : null; // find the function to call
-                    //dd('here' . $accessLevel);
+            $accessCheckDelegate = null; // Default is to deny access
+            foreach ($sessionUser->roles as $r) { // %FIXME: BUG this will only check the first role it finds assoc w/ the user (write test to expose)
+                if ( array_key_exists($route,$accessMatrix) ) {
+                    $accessCheckDelegate = !empty($accessMatrix[$route][$r->name]) ? $accessMatrix[$route][$r->name] : null; // find the function to cll
                     break;
-    
                 }
             }
     
             // If we get through the case statement without aborting, access is allowed
-            if ( is_callable($accessLevel) ) {
-                $isAllowed = $accessLevel($sessionUser,$routeParams,$queryParams); // call function
+            if ( is_callable($accessCheckDelegate) ) {
+                $isAllowed = call_user_func_array($accessCheckDelegate,[$sessionUser,$routeParams,$queryParams]); // call function
             } else {
-                switch ($accessLevel) {
+                switch ($accessCheckDelegate) {
                     case 'all':
                         $isAllowed = true; // continue => grant access
                         break;
@@ -88,9 +70,8 @@ abstract class AccessManager
                         $isAllowed = false;
                 }
             }
-            //dd($route,$sessionUser->email,$accessLevel,$role->name,$roles->toArray());
             if (!$isAllowed) {
-                $jsonStr = json_encode(['route'=>$route,'user'=>$sessionUser->username,'roles'=>$roles]);
+                $jsonStr = json_encode(['route'=>$route,'user'=>$sessionUser->username,'roles'=>$sessionUser->roles]);
                 \App::abort(403, 'Unauthorized action for role: '.$jsonStr);
             }
             // else allowed, return....
@@ -102,3 +83,21 @@ abstract class AccessManager
     
     
 }
+
+// See: 
+// ~ => https://laravel.com/docs/5.6/authentication#protecting-routes
+// ~ https://laravel.com/docs/5.6/authorization
+// ~ https://github.com/Zizaco/entrust
+// ~ https://stackoverflow.com/questions/33439193/in-laravel-should-i-check-for-permission-in-controller-if-already-checking-on-m?rq=1
+// ~ https://github.com/spatie/laravel-authorize
+
+/*
+//$tmp = $request->route()->parameters();
+//dd($tmp);
+//dd($tmp->parameters());
+//dd($request->route('contractors'));
+//$slug = $request->slug; //  get parameter from request (eg slug)
+//$route = $request->route;
+//dd($route,$sessionUser->email,$accessLevel,$role->name,$sessionUser->roles->pluck('name'));
+//dd($route);
+ */
